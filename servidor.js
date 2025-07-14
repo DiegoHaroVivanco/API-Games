443f05242f4f940b9f26290b5991500c96af257c
Diego Haro Vivanco <diego.harovivanco@gmail.com>
2025-07-14 19:11:52 -0300

    Configuracion basica del servidor de express

---
+++ servidor.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const {testConexion} = require('./config/baseDatos')
+
+
+const app = express();
+const puerto = process.env.PORT || 3000;
+
+app.use(cors()); // Para que el servidor reciba peticiones desde otros dominios
+app.use(express.json());
+
+// app.get('/', (req, res)=>{
+//     res.json({
+//         mensaje: 'API de juegos funcionando.',
+//         estado: 'Servidor activo'
+//     })
+// })
+
+app.get('/', (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>API GAMES</title>
+            <style>
+                html {
+                    height: 100%;
+                }
+                body {
+                    margin: 0;
+                    background-color: bisque;
+                    color: black;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100%;
+                    font-family: 'Courier New', Courier, monospace;
+                    text-align: center;
+                }
+            </style>
+        </head>
+        <body>
+            <div>
+                <h1>API de videojuegos funcionado</h1>
+                <p>Servidor activo</p>
+            </div>
+        </body>
+        </html>`)
+})
+
+const iniciarServer = async ()=>{
+    try {
+        await testConexion();
+
+        app.listen(puerto, () => {
+            console.log(`Servidor escuchando en http://localhost:${puerto}`)
+        })
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+iniciarServer();
